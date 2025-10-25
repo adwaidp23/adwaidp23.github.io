a3d90514a9837ebc9177b30dd323d648544cd5a6
adwaidp23 <adwaidp08@gmail.com>
2025-10-25 21:37:23 +0530

    Update and rename js/main.js to script.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Contact form submission (demo only)
-const contactForm = document.getElementById('contactForm');
-contactForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for your message!');
-  contactForm.reset();
-});
